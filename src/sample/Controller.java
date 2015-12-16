ce2b2ee8683725ceeff2e384f850bcdb5b1e80b9
Jerry <Jeremias.Boos@gmx.de>
2015-12-16 03:02:13 +0100

    Lets see some graphs

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,7 +1,10 @@
 package sample;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.ObservableListBase;
 import javafx.fxml.FXML;
-import javafx.scene.chart.BarChart;
+import javafx.scene.chart.*;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.FileChooser;
 import org.w3c.dom.Document;
@@ -25,10 +28,14 @@ public class Controller {
     private BorderPane mainStage;
 
     @FXML
-    private BarChart killchart;
+    private StackedBarChart killchart;
+
+    @FXML
+    private BarChart scores;
 
     private HashMap<String,Vector<int[]>> mehrhuhndeaths = new HashMap<>();
     private HashMap<String,Vector<Integer>> score = new HashMap<>();
+
 
     @FXML
     private void loadLog(){
@@ -36,6 +43,8 @@ public class Controller {
         fileChooser.setTitle("Open Resource File");
         fileChooser.setInitialDirectory(new File("I:\\Users\\Jerry\\Documents\\Tests"));
         List<File> files = fileChooser.showOpenMultipleDialog(mainStage.getScene().getWindow());
+
+
 
         String level;
         for(File file : files)
@@ -93,6 +102,18 @@ public class Controller {
             }
 
         }
+
+        ObservableList<XYChart.Series<String,Number>> level_data = (killchart.getData() == null?FXCollections.observableArrayList():killchart.getData());
+        XYChart.Series<String, Number> s_data_025p = new XYChart.Series<>();
+        s_data_025p.setName("25P");
+
+        XYChart.Series<String, Number> s_data_050p = new XYChart.Series<>();
+        s_data_050p.setName("50P");
+
+        XYChart.Series<String, Number> s_data_100p = new XYChart.Series<>();
+        s_data_100p.setName("100P");
+
+
         for(String key : mehrhuhndeaths.keySet()){
             float[] kill_count = new float[3];
             for(int[] kills : mehrhuhndeaths.get(key)){
@@ -101,17 +122,48 @@ public class Controller {
                 kill_count[2] += kills[2];
             }
 
+            s_data_025p.getData().add(new XYChart.Data<>(key, kill_count[0]));
+            s_data_050p.getData().add(new XYChart.Data<>(key, kill_count[1]));
+            s_data_100p.getData().add(new XYChart.Data<>(key, kill_count[2]));
+
             System.out.println("Level "+ key+ " 25p: "+(kill_count[0]/mehrhuhndeaths.get(key).size())+ " 50p: "+(kill_count[1]/mehrhuhndeaths.get(key).size())+ " 100p: "+(kill_count[2]/mehrhuhndeaths.get(key).size()));
         }
+        level_data.add(s_data_025p);
+        level_data.add(s_data_050p);
+        level_data.add(s_data_100p);
+        killchart.setData(level_data);
+
+
+        ObservableList<XYChart.Series<String,Number>> score_data = (scores.getData() == null?FXCollections.observableArrayList():scores.getData());
+        XYChart.Series<String, Number> s_data_min = new XYChart.Series<>();
+        s_data_min.setName("Min Score");
+
+        XYChart.Series<String, Number> s_data_med = new XYChart.Series<>();
+        s_data_med.setName("Medium Score");
+
+        XYChart.Series<String, Number> s_data_max = new XYChart.Series<>();
+        s_data_max.setName("Max Score");
 
         for(String key : score.keySet()){
 
             System.out.println("Level "+ key);
+
+            s_data_max.getData().add(new XYChart.Data<>(key,score.get(key).get(0)));
+            s_data_min.getData().add(new XYChart.Data<>(key,score.get(key).get(score.get(key).size()-1)));
+
+            float med = 0;
             for(Integer points : score.get(key))
             {
+                med += points;
                 System.out.println("Score: "+ points);
             }
-
+            med /= score.get(key).size();
+            s_data_med.getData().add(new XYChart.Data<>(key,med));
         }
+
+        score_data.add(s_data_min);
+        score_data.add(s_data_med);
+        score_data.add(s_data_max);
+        scores.setData(score_data);
     }
 }
